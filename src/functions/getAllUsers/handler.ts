74cc0013f054d7174916ba2555d223cc84b4f346
m-yasir0 <m.yasir3497@gmail.com>
2021-10-25 19:47:16 +0500

    crud with test jest

--- src/functions/getAllUsers/handler.ts
+++ src/functions/getAllUsers/handler.ts
@@ -8,21 +8,23 @@ import { DynameDb } from '../../common/dynamo.class';
 
 import schema from './schema';
 
-const getRecordById: ValidatedEventAPIGatewayProxyEvent<typeof schema> =
-  async () => {
-    try {
-      let record = await new DynameDb(
-        process.env.IS_OFFLINE,
-        'Users'
-      ).getAllRecords();
-      return _200_formatJSONResponse({
-        Users: record,
-      });
-    } catch (e) {
-      return _400_formatJSONResponse({
-        Error: e,
-      });
-    }
-  };
+const getRecordById: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (
+  event
+) => {
+  event;
+  try {
+    let record = await new DynameDb(
+      process.env.IS_OFFLINE,
+      'Users'
+    ).getAllRecords();
+    return _200_formatJSONResponse({
+      Users: record,
+    });
+  } catch (e) {
+    return _400_formatJSONResponse({
+      Error: e,
+    });
+  }
+};
 
 export const main = middyfy(getRecordById);
